06dc1966db29fbcdbeed8bc1f4b20dda3b96d6df
Professional <new99kpicem@gmail.com>
2023-06-12 21:33:38 +0300

    initial

--- src/main/java/course2/homework2_11/services/CartService.java
+++ src/main/java/course2/homework2_11/services/CartService.java
@@ -1,15 +1,16 @@
 package course2.homework2_11.services;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.SessionScope;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 @Service
+@SessionScope
 public class CartService {
     private final List<Integer> storage = new ArrayList<>();
-
     public void add(List<Integer> ids) {
         storage.addAll(ids);
     }
